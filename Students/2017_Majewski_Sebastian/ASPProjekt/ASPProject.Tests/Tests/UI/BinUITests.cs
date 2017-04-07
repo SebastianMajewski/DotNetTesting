40116832b79d1cf2152eedb54c2dd6eda4dc67c2
Sebastian Majewski <sebastian.majewski.it@gmail.com>
2017-04-07 10:14:11 +0200

    Selenium Tests

--- Students/2017_Majewski_Sebastian/ASPProjekt/ASPProject.Tests/Tests/UI/BinUITests.cs
+++ Students/2017_Majewski_Sebastian/ASPProjekt/ASPProject.Tests/Tests/UI/BinUITests.cs
@@ -1,116 +1,105 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace ASPProject.Tests.Tests.UI
-//{
-//    using System.Collections.ObjectModel;
+namespace ASPProject.Tests.Tests.UI
+{
+    using System.Collections.ObjectModel;
 
-//    using OpenQA.Selenium;
-//    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
 
-//    [TestClass]
-//    public class BinUITests
-//    {
-//        [TestClass]
-//        public class SeleniumPerson
-//        {
-//            private IWebDriver driver;
-//            private INavigation navigation;
+    [TestClass]
+    public class BinUITests : SeleniumHelper
+    {
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            SeleniumHelper.ClassInitialize(Logins.UserOne);
+        }
 
-//            [TestInitialize]
-//            public void SetUp()
-//            {
-//                this.driver = new ChromeDriver();
-//                this.navigation = driver.Navigate();
-//                this.navigation.GoToUrl("http://localhost:9098/Person/All");
-//            }
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            SeleniumHelper.ClassCleanup();
+        }
 
-//            [TestCleanup]
-//            public void CleanUp()
-//            {
-//                this.driver.Close();
-//            }
+        [TestMethod]
+        public void AddBinRedirect()
+        {
+            this.Open("~/Bins/Create");
+            Assert.AreEqual("Nowy śmietnik", this.GetElement("title").Text);
+            this.Type("name", "Bin");
+            this.Type("Description", "New Bin");
+            this.Click("submitBin");
+            Assert.AreEqual("Śmietniki użytkownika:", this.GetElement("bins").Text);
+        }
 
-//            [TestMethod]
-//            public void TestClickSprawdzAdres()
-//            {
-//                IWebDriver driver = new ChromeDriver();
-//                INavigation nav = driver.Navigate();
-//                nav.GoToUrl("http://localhost:9098/Person/All");
-//                IWebElement element = driver.FindElement(By.Name("sprAdres"));
-//                element.Click();
-//                string checkTest = driver.FindElement(By.Id("Title")).Text;
-//                Assert.AreEqual("Adres osoby", checkTest);
-//                driver.Close();
-//            }
+        [TestMethod]
+        public void BinDelete()
+        {
+            this.Open("~/Bins/Create");
+            this.Type("name", "Bin");
+            this.Type("Description", "New Bin");
+            this.Click("submitBin");
+            this.Open("~/Bins/Delete/1");
+            Assert.AreEqual("Usuń śmietnik", this.GetElement("title").Text);
+            this.Click("submit");
+            Assert.AreEqual("Śmietniki użytkownika:", this.GetElement("bins").Text);
+        }
 
-//            [TestMethod]
-//            public void TestClickUsun()
-//            {
-//                IWebDriver driver = new ChromeDriver();
-//                INavigation nav = driver.Navigate();
-//                nav.GoToUrl("http://localhost:9098/Person/All");
-//                IWebElement element = driver.FindElement(By.Name("usunPerson"));
-//                element.Click();
-//                string checkTest = driver.FindElement(By.Id("Title")).Text;
-//                Assert.AreEqual("Usuwanie osoby", checkTest);
-//                driver.Close();
-//            }
+        [TestMethod]
+        public void BinEdit()
+        {
+            IWebDriver driver = new ChromeDriver("./", new ChromeOptions(), TimeSpan.FromSeconds(300));
+            INavigation nav = driver.Navigate();
+            nav.GoToUrl("http://www.google.com");
+            Assert.IsTrue(true);
+            //IWebElement el = driver.FindElement(By.Name("edytuj"));
+            //String message = el.Text;
+            //String successMsg = "Edytuj dane";
+            //Assert.AreEqual(message, successMsg);
+        }
 
-//            [TestMethod]
-//            public void TestTextEdytuj()
-//            {
-//                IWebDriver driver = new ChromeDriver();
-//                INavigation nav = driver.Navigate();
-//                nav.GoToUrl("http://localhost:9098/Person/All");
-//                IWebElement el = driver.FindElement(By.Name("edytuj"));
-//                String message = el.Text;
-//                String successMsg = "Edytuj dane";
-//                Assert.AreEqual(message, successMsg);
-//                driver.Close();
-//            }
+        [TestMethod]
+        public void TestAddPerson()
+        {
+            IWebDriver driver = new ChromeDriver();
+            INavigation nav = driver.Navigate();
+            nav.GoToUrl("http://localhost:9098/Person/Add");
+            driver.FindElement(By.Id("nazwisko")).Click();
+            driver.FindElement(By.Id("nazwisko")).SendKeys("Nowak");
+            driver.FindElement(By.Id("imie")).Click();
+            driver.FindElement(By.Id("imie")).SendKeys("Kasia");
+            driver.FindElement(By.Name("dodaj")).Click();
+            string checkTest = driver.FindElement(By.Name("Title")).Text;
+            Assert.AreEqual("Lista wszystkich osób", checkTest);
+            driver.Close();
+        }
 
-//            [TestMethod]
-//            public void TestAddPerson()
-//            {
-//                IWebDriver driver = new ChromeDriver();
-//                INavigation nav = driver.Navigate();
-//                nav.GoToUrl("http://localhost:9098/Person/Add");
-//                driver.FindElement(By.Id("nazwisko")).Click();
-//                driver.FindElement(By.Id("nazwisko")).SendKeys("Nowak");
-//                driver.FindElement(By.Id("imie")).Click();
-//                driver.FindElement(By.Id("imie")).SendKeys("Kasia");
-//                driver.FindElement(By.Name("dodaj")).Click();
-//                string checkTest = driver.FindElement(By.Name("Title")).Text;
-//                Assert.AreEqual("Lista wszystkich osób", checkTest);
-//                driver.Close();
-//            }
+        [TestMethod]
+        public void TestAllPerson()
+        {
+            IWebDriver driver = new ChromeDriver();
+            INavigation nav = driver.Navigate();
+            nav.GoToUrl("http://localhost:9098/Person/All");
 
-//            [TestMethod]
-//            public void TestAllPerson()
-//            {
-//                IWebDriver driver = new ChromeDriver();
-//                INavigation nav = driver.Navigate();
-//                nav.GoToUrl("http://localhost:9098/Person/All");
+            IWebElement table = driver.FindElement(By.XPath("/html/body/div[2]/table"));
 
-//                IWebElement table = driver.FindElement(By.XPath("/html/body/div[2]/table"));
+            ReadOnlyCollection<IWebElement> allRows = table.FindElements(By.TagName("tr"));
+            int licz = 0;
+            foreach (IWebElement row in allRows)
+            {
+                ReadOnlyCollection<IWebElement> cells = row.FindElements(By.TagName("td"));
 
-//                ReadOnlyCollection<IWebElement> allRows = table.FindElements(By.TagName("tr"));
-//                int licz = 0;
-//                foreach (IWebElement row in allRows)
-//                {
-//                    ReadOnlyCollection<IWebElement> cells = row.FindElements(By.TagName("td"));
+                foreach (IWebElement cell in cells)
+                {
+                    //Console.WriteLine("\t" + cell.Text);
+                }
+                licz++;
+            }
+            Assert.IsTrue(licz > 4);
+            driver.Close();
+        }
 
-//                    foreach (IWebElement cell in cells)
-//                    {
-//                        //Console.WriteLine("\t" + cell.Text);
-//                    }
-//                    licz++;
-//                }
-//                Assert.IsTrue(licz > 4);
-//                driver.Close();
-//            }
-
-//        }
-//    }
-//}
+    }
+}
--- Students/2017_Majewski_Sebastian/ASPProjekt/ASPProject.Tests/Tests/UI/SeleniumHelper.cs
+++ Students/2017_Majewski_Sebastian/ASPProjekt/ASPProject.Tests/Tests/UI/SeleniumHelper.cs
@@ -1,205 +1,207 @@
 ﻿
-//namespace ASPProject.Tests.Tests.UI
-//{
-//    using System;
+namespace ASPProject.Tests.Tests.UI
+{
+    using System;
 
-//    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//    using OpenQA.Selenium;
-//    using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
 
-//    public class SeleniumHelper
-//    {
-//        public const string BaseUrl = "http://localhost:8888";
-//        private const string VirtualPath = "";
-//        private const int TimeOut = 30;
-//        private static readonly IWebDriver StaticDriver = CreateDriverInstance();
-//        private static Login currentlyLoggedInAs;
-//        private static int testClassesRunning;
+    public class SeleniumHelper
+    {
+        public const string BaseUrl = "http://aspprojekt.azurewebsites.net";
+        private const string VirtualPath = "";
+        private const int TimeOut = 1000;
+        private static readonly IWebDriver StaticDriver = CreateDriverInstance();
+        private static Login currentlyLoggedInAs;
+        private static int testClassesRunning;
 
-//        static SeleniumHelper()
-//        {
-//            StaticDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(TimeOut);
-//        }
+        static SeleniumHelper()
+        {
+            StaticDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(TimeOut);
+            StaticDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(TimeOut);
+            StaticDriver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(TimeOut);
+        }
 
-//        public IWebDriver Driver => StaticDriver;
+        public IWebDriver Driver => StaticDriver;
 
-//        // Pass in null if want to run your test-case without logging in.
-//        public static void ClassInitialize(Login login = null)
-//        {
-//            testClassesRunning++;
-//            if (login == null)
-//            {
-//                Logoff();
-//            }
-//            else if (!IsCurrentlyLoggedInAs(login))
-//            {
-//                Logon(login);
-//            }
-//        }
+        // Pass in null if want to run your test-case without logging in.
+        public static void ClassInitialize(Login login = null)
+        {
+            testClassesRunning++;
+            if (login == null)
+            {
+                Logoff();
+            }
+            else if (!IsCurrentlyLoggedInAs(login))
+            {
+                Logon(login);
+            }
+        }
 
-//        public static void ClassCleanup()
-//        {
-//            try
-//            {
-//                testClassesRunning--;
-//                if (testClassesRunning == 0)
-//                {
-//                    StaticDriver.Quit();
-//                }
-//            }
-//            catch (Exception)
-//            {
-//                // Ignore errors if unable to close the browser
-//            }
-//        }
+        public static void ClassCleanup()
+        {
+            try
+            {
+                testClassesRunning--;
+                if (testClassesRunning == 0)
+                {
+                    StaticDriver.Close();
+                }
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
 
-//        public void Open(string url)
-//        {
-//            this.Driver.Navigate().GoToUrl(BaseUrl + VirtualPath + url.Trim('~'));
-//        }
+        public void Open(string url)
+        {
+            this.Driver.Navigate().GoToUrl(BaseUrl + VirtualPath + url.Trim('~'));
+        }
 
-//        public void Click(string id)
-//        {
-//            this.Click(By.Id(id));
-//        }
+        public void Click(string id)
+        {
+            this.Click(By.Id(id));
+        }
 
-//        public void Click(By locator)
-//        {
-//            this.Driver.FindElement(locator).Click();
-//        }
+        public void Click(By locator)
+        {
+            this.Driver.FindElement(locator).Click();
+        }
 
-//        // public void ClickAndWait(string id, string newUrl)
-//        // {
-//        //   ClickAndWait(By.Id(id), newUrl);
-//        // }
+        // public void ClickAndWait(string id, string newUrl)
+        // {
+        //   ClickAndWait(By.Id(id), newUrl);
+        // }
 
-//        // <summary>
-//        // Use when you are navigating via a hyper-link and need for the page to fully load before 
-//        // moving further.  
-//        // </summary>
-//        // public void ClickAndWait(By locator, string newUrl)
-//        // {
-//        // Driver.FindElement(locator).Click();
-//        // WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(TimeOut));
-//        // wait.Until(d => d.Url.Contains(newUrl.Trim('~')));
-//        // }
+        // <summary>
+        // Use when you are navigating via a hyper-link and need for the page to fully load before 
+        // moving further.  
+        // </summary>
+        // public void ClickAndWait(By locator, string newUrl)
+        // {
+        // Driver.FindElement(locator).Click();
+        // WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(TimeOut));
+        // wait.Until(d => d.Url.Contains(newUrl.Trim('~')));
+        // }
 
-//        public void AssertCurrentPage(string pageUrl)
-//        {
-//            var absoluteUrl = new Uri(new Uri(BaseUrl), VirtualPath + pageUrl.Trim('~')).ToString();
-//            Assert.AreEqual(absoluteUrl, Driver.Url);
-//        }
+        public void AssertCurrentPage(string pageUrl)
+        {
+            var absoluteUrl = new Uri(new Uri(BaseUrl), VirtualPath + pageUrl.Trim('~')).ToString();
+            Assert.AreEqual(absoluteUrl, Driver.Url);
+        }
 
-//        public void AssertTextContains(string id, string text)
-//        {
-//            this.AssertTextContains(By.Id(id), text);
-//        }
+        public void AssertTextContains(string id, string text)
+        {
+            this.AssertTextContains(By.Id(id), text);
+        }
 
-//        public void AssertTextContains(By locator, string text)
-//        {
-//            Assert.IsTrue(this.Driver.FindElement(locator).Text.Contains(text));
-//        }
+        public void AssertTextContains(By locator, string text)
+        {
+            Assert.IsTrue(this.Driver.FindElement(locator).Text.Contains(text));
+        }
 
-//        public void AssertTextEquals(string id, string text)
-//        {
-//            this.AssertTextEquals(By.Id(id), text);
-//        }
+        public void AssertTextEquals(string id, string text)
+        {
+            this.AssertTextEquals(By.Id(id), text);
+        }
 
-//        public void AssertTextEquals(By locator, string text)
-//        {
-//            Assert.AreEqual(text, this.Driver.FindElement(locator).Text);
-//        }
+        public void AssertTextEquals(By locator, string text)
+        {
+            Assert.AreEqual(text, this.Driver.FindElement(locator).Text);
+        }
 
-//        public void AssertValueContains(string id, string text)
-//        {
-//            this.AssertValueContains(By.Id(id), text);
-//        }
+        public void AssertValueContains(string id, string text)
+        {
+            this.AssertValueContains(By.Id(id), text);
+        }
 
-//        public void AssertValueContains(By locator, string text)
-//        {
-//            Assert.IsTrue(this.GetValue(locator).Contains(text));
-//        }
+        public void AssertValueContains(By locator, string text)
+        {
+            Assert.IsTrue(this.GetValue(locator).Contains(text));
+        }
 
-//        public void AssertValueEquals(string id, string text)
-//        {
-//            this.AssertValueEquals(By.Id(id), text);
-//        }
+        public void AssertValueEquals(string id, string text)
+        {
+            this.AssertValueEquals(By.Id(id), text);
+        }
 
-//        public void AssertValueEquals(By locator, string text)
-//        {
-//            Assert.AreEqual(text, GetValue(locator));
-//        }
+        public void AssertValueEquals(By locator, string text)
+        {
+            Assert.AreEqual(text, GetValue(locator));
+        }
 
-//        public IWebElement GetElement(string id)
-//        {
-//            return Driver.FindElement(By.Id(id));
-//        }
+        public IWebElement GetElement(string id)
+        {
+            return Driver.FindElement(By.Id(id));
+        }
 
-//        public string GetValue(By locator)
-//        {
-//            return Driver.FindElement(locator).GetAttribute("value");
-//        }
+        public string GetValue(By locator)
+        {
+            return Driver.FindElement(locator).GetAttribute("value");
+        }
 
-//        public string GetText(By locator)
-//        {
-//            return Driver.FindElement(locator).Text;
-//        }
+        public string GetText(By locator)
+        {
+            return Driver.FindElement(locator).Text;
+        }
 
-//        public void Type(string id, string text)
-//        {
-//            var element = GetElement(id);
-//            element.Clear();
-//            element.SendKeys(text);
-//        }
+        public void Type(string id, string text)
+        {
+            var element = GetElement(id);
+            element.Clear();
+            element.SendKeys(text);
+        }
 
-//        public void Uncheck(string id)
-//        {
-//            Uncheck(By.Id(id));
-//        }
+        public void Uncheck(string id)
+        {
+            Uncheck(By.Id(id));
+        }
 
-//        public void Uncheck(By locator)
-//        {
-//            var element = Driver.FindElement(locator);
-//            if (element.Selected) element.Click();
-//        }
+        public void Uncheck(By locator)
+        {
+            var element = Driver.FindElement(locator);
+            if (element.Selected) element.Click();
+        }
 
-//        // Selects an element from a drop-down list.
-//        public void Select(string id, string valueToBeSelected)
-//        {
-//            var options = GetElement(id).FindElements(By.TagName("option"));
-//            foreach (var option in options)
-//            {
-//                if (valueToBeSelected == option.Text) option.Click();
-//            }
-//        }
+        // Selects an element from a drop-down list.
+        public void Select(string id, string valueToBeSelected)
+        {
+            var options = GetElement(id).FindElements(By.TagName("option"));
+            foreach (var option in options)
+            {
+                if (valueToBeSelected == option.Text) option.Click();
+            }
+        }
 
-//        private static IWebDriver CreateDriverInstance(string baseUrl = BaseUrl)
-//        {
-//            return new FirefoxDriver();
-//        }
+        private static IWebDriver CreateDriverInstance(string baseUrl = BaseUrl)
+        {
+            return new ChromeDriver("./", new ChromeOptions(), TimeSpan.FromSeconds(300));
+        }
 
-//        private static bool IsCurrentlyLoggedInAs(Login login)
-//        {
-//            return currentlyLoggedInAs != null && currentlyLoggedInAs.Equals(login);
-//        }
+        private static bool IsCurrentlyLoggedInAs(Login login)
+        {
+            return currentlyLoggedInAs != null && currentlyLoggedInAs.Equals(login);
+        }
 
-//        private static void Logon(Login login)
-//        {
-//            StaticDriver.Navigate().GoToUrl(BaseUrl + VirtualPath + "/Logon.aspx");
+        private static void Logon(Login login)
+        {
+            StaticDriver.Navigate().GoToUrl(BaseUrl + "/Account/Login");
 
-//            StaticDriver.FindElement(By.Id("userId")).SendKeys(login.Username);
-//            StaticDriver.FindElement(By.Id("password")).SendKeys(login.Password);
-//            StaticDriver.FindElement(By.Id("btnLogin")).Click();
+            StaticDriver.FindElement(By.Id("email")).SendKeys(login.Username);
+            StaticDriver.FindElement(By.Id("password")).SendKeys(login.Password);
+            StaticDriver.FindElement(By.Id("loginbutton")).Click();
+            currentlyLoggedInAs = login;
+        }
 
-//            currentlyLoggedInAs = login;
-//        }
-
-//        private static void Logoff()
-//        {
-//            StaticDriver.Navigate().GoToUrl(VirtualPath + RedirectLinks.SignOff.Trim('~'));
-//            currentlyLoggedInAs = null;
-//        }
-//    }
-//}
+        private static void Logoff()
+        {
+            StaticDriver.FindElement(By.Id("logout")).Click();
+            currentlyLoggedInAs = null;
+        }
+    }
+}
 
